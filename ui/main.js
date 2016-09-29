43dcb5ee42511c351f38561a871037868e40430e
onam24 <onam.bhartia@gmail.com>
2016-09-29 21:24:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,6 @@ element.innerHTML = 'New Value';
 
 //move the image
 var img = document.getElementById('madi');
-img.onclick = function(){
+img.onclick = function () {
     img.style.leftMargin = '100px';
 };
